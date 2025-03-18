1c6f0da7c57ecb08fa039a9c166fa3ce3dd56c0a
Michael <michaelflopez10@gmail.com>
2025-03-18 09:41:33 +0100

    del: delete scraper and using only requests

--- my_project/app/main.py
+++ my_project/app/main.py
@@ -1,14 +1,48 @@
 from fastapi import FastAPI
-from scraper.scraper import getCryptoData
-from fastapi.responses import JSONResponse
+import pandas as pd
+import requests
+from pydantic import BaseModel
 
 app = FastAPI()
 
+API_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 
-@app.get("/cryptos")
-async def getCryptos():
-    data = getCryptoData()
-    if data is not None:
-        return JSONResponse(content=data.to_dict(orient="records"))
+API_KEY = '711ceffc-6d4a-4fa7-870f-0e6d48df306f'
+
+parameters = {
+    'start': '1',
+    'limit': '10',
+    'convert': 'USD'
+}
+
+headers = {
+    'Accepts': 'aplication/json',
+    'X-CMC_PRO_API_KEY': API_KEY
+}
+
+class cryptoResponse(BaseModel):
+    name: str
+    symbol: str
+    price: float
+
+@app.get('/cryptos', response_model=list[cryptoResponse])
+async def get_cryptocurrencies():
+    response = requests.get(API_URL, params=parameters, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        
+        crypto_data = []
+        for item in data['data']:
+            crypto_data.append({
+                'name': item['name'],
+                'symbol': item['symbol'],
+                'price': item['quote']['USD']['price']
+            })
+
+        df = pd.DataFrame(crypto_data)
+        
+        
+        return df.to_dict(orient='records')
     else:
-        return{"error": "No se pudo obtener datos de las criptomonedas"}
+        return {"error": "Failed to fetch data from CoinMarketCap API", "status_code": response.status_code} 
